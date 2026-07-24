738366ac3dc4a92fb9e68f2251b34bcb9c345f87
Jose Luis Valera <josel.valera@gmail.com>
2026-07-24 18:55:18 +0200

    Actualizacion 2026-07-24

--- sw.js
+++ sw.js
@@ -14,7 +14,7 @@
  */
 'use strict';
 
-const VERSION = 'docentes-v23';
+const VERSION = 'docentes-v24';
 const CACHE_SHELL = 'shell-' + VERSION;
 const CACHE_DATOS = 'datos-' + VERSION;
 const PRECACHE = [
